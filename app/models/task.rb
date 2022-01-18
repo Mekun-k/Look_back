c7803ade0195612ed9978474e982d29a4ab659a9
Mekun-k <akaidonnbei@gmail.com>
2022-01-18 23:47:40 +0900

    Add: リレーション、パリデーション実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+    belongs_to :user
+
+    validates :name, presence: true, length: { maximum: 30 }
+    validates :body, presence: true, length: { maximum: 50 }
+    validates :task_url, presence: true, length: { maximum: 50 }
+    validates :task_state, presence: true
+    validates :start_date, presence: true
+    validates :doing_date, presence: true
+
+
 end
