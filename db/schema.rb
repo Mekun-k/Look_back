976ec388d2acb11eeaff8bd5d8f0ca6e12b788e3
Mekun-k <akaidonnbei@gmail.com>
2022-02-09 21:00:21 +0900

    Add: articleテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_125041) do
+ActiveRecord::Schema.define(version: 2022_02_09_114804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.integer "qiita_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_articles_on_task_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.bigint "user_id"
@@ -56,5 +64,6 @@ ActiveRecord::Schema.define(version: 2022_01_18_125041) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "articles", "tasks"
   add_foreign_key "tasks", "users"
 end
