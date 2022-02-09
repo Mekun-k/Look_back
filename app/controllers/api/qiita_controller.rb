167ad49fb8db99d1d42426cf90e4c19fed61b71b
Mekun-k <akaidonnbei@gmail.com>
2022-02-09 18:12:56 +0900

    Edit: URL変更

--- app/controllers/api/qiita_controller.rb
+++ app/controllers/api/qiita_controller.rb
@@ -3,16 +3,9 @@ class Api::QiitaController < ApplicationController
   require 'httpclient'
 
   def index
-    url = "https://qiita.com/api/v2/users/akaidonnbei/items"  # URLを設定 記事の一覧を作成日時の降順で返します。
+    url = "https://qiita.com/api/v2/users/akaidonnbei/stocks"  # URLを設定 記事の一覧を作成日時の降順で返します。
     client = HTTPClient.new                 # インスタンスを生成
     response = client.get(url)              # Getリクエスト
     render json: JSON.parse(response.body)  # 結果をjsonにパースして表示
   end
-
-  def new
-    url = "https://qiita.com/api/v2/users/akaidonnbei/stocks"  # URLを設定 ストック記事の一覧を作成日時の降順で返します。
-    client = HTTPClient.new                 # インスタンスを生成
-    response = client.get(url)              # Getリクエスト
-    render json: JSON.parse(response.body)  # 結果をjsonにパースして表示
-  end
-end
+endgit
