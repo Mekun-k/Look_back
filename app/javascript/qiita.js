62adaa27297cb30ad3248722f55f8430800fb2cb
Mekun-k <akaidonnbei@gmail.com>
2022-02-27 14:21:52 +0900

    Add: 記事id

--- app/javascript/qiita.js
+++ app/javascript/qiita.js
@@ -1,12 +1,11 @@
 import { end } from "@popperjs/core";
 
 $(document).on ("turbolinks:load", function(){
-    //ボタンが押されたら入力内容をアラートで表示する
     $('#task_select-perent').change(function(){
         var text = $('option:selected').val();
-        console.log(text);
         if (text == 1){
-            console.log("ユーザ一覧");
+        // Qiitaユーザー記事がクリックされた時の処理
+
             $.ajax({
                 // 読み込みの設定
                 type: "GET",
@@ -16,12 +15,30 @@ $(document).on ("turbolinks:load", function(){
             }).then(
                 function (items_data) {
                     $('#task_select-children').children('option').remove();
+                    $('#task_qiita_id').children('option').remove();
                     // 最初のoption要素削除
                     $.each(items_data, function(key, value) {
-                        console.log(key + ':' + value.title + ':' + value.url);
-                        // デバッグ用
-                        $('#task_select-children').append("<option value='" + value.title + "'>" + value.title + "</option>");
+                        $('#task_select-children').append("<option value='" + key + "'>" + value.title + "</option>");
                         // タイトルを出力
+                    });
+
+                    $('#task_select-children').change(function(){
+                        // 二つ目のセレクトボックスを変更したとき
+
+                        var text = $('[name="task[select-children]"] option:selected').val();
+                        // 選択した記事のvalueを取得
+
+                        var choice = $(items_data[text]);
+                        // valueから記事のデータ取得
+
+                        $('#task_qiita_id').children('option').remove();
+                        // 最初のoption要素削除
+
+                        $.each(choice, function(key, value) {
+                        // idを取得するためにeachメソッド使用
+                              $('#task_qiita_id').append("<option value='" + value.id + "'>" + value.id + "</option>");
+                               // optionに記事idを表示
+                        });
                     });
                 },
                 function () {
@@ -29,8 +46,9 @@ $(document).on ("turbolinks:load", function(){
                     console.log("読み込みに失敗しました");
                 }
             );
-        }else{
-            console.log("ストック一覧");
+
+        }else if (text == 2){
+        // Qiitaストック記事がクリックされた時の処理
             $.ajax({
                 // 読み込みの設定
                 type: "GET",
@@ -40,12 +58,33 @@ $(document).on ("turbolinks:load", function(){
             }).then(
                 function (stocks_data) {
                     $('#task_select-children').children('option').remove();
+                    $('#task_qiita_id').children('option').remove();
+
                     // 最初のoption要素削除
                     $.each(stocks_data, function(key, value) {
-                        console.log(key + ':' + value.title + ':' + value.url);
+                        // console.log(key + ':' + value.title + ':' + value.url);
                         // デバッグ用
-                        $('#task_select-children').append("<option value='" + value.title + "'>" + value.title + "</option>");
+                        $('#task_select-children').append("<option value='" + key + "'>" + value.title + "</option>");
                         // タイトルを出力
+                    });
+
+                    $('#task_select-children').change(function(){
+                        // 二つ目のセレクトボックスを変更したとき
+
+                        var text = $('[name="task[select-children]"] option:selected').val();
+                        // 選択した記事のvalueを取得
+
+                        var choice = $(stocks_data[text]);
+                        // valueから記事のデータ取得
+
+                        $('#task_qiita_id').children('option').remove();
+                        // 最初のoption要素削除
+
+                        $.each(choice, function(key, value) {
+                        // idを取得するためにeachメソッド使用
+                              $('#task_qiita_id').append("<option value='" + value.id + "'>" + value.id + "</option>");
+                               // optionに記事idを表示
+                        });
                     });
                 },
                 function () {
@@ -53,6 +92,13 @@ $(document).on ("turbolinks:load", function(){
                     console.log("読み込みに失敗しました");
                 }
             );
+
+        } else {
+        // 「選択してください」がクリックされた時の処理
+
+            $('#task_select-children').children('option').remove();
+            $('#task_qiita_id').children('option').remove();
+            // 記事選択、Qiitaのoptionリセット
         }
     });
 });
