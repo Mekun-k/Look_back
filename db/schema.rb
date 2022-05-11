1c4dd7fbac8e173f5355ff18ec9426801c34817b
Mekun-k <akaidonnbei@gmail.com>
2022-05-12 07:34:30 +0900

    Add: tasksテーブルにcompleted_flagを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_23_040250) do
+ActiveRecord::Schema.define(version: 2022_05_11_223137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2022_04_23_040250) do
     t.integer "task_state", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed_flag", default: false, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
